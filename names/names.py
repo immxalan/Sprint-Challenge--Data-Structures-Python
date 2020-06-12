57053452cfc675bbdd1229adc0f4feeba95b8bbe
immxalan <aedocoding@gmail.com>
2020-06-12 09:38:24 -0700

    Names done

--- names/names.py
+++ names/names.py
@@ -1,14 +1,14 @@
 import time
-# import sys
-# from bst import BSTNode
-# sys.path.append('../bst')
-# start_time = time.time()
+import sys
+from bst import BSTNode
+sys.path.append('../bst')
+start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('./names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('./names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
@@ -19,7 +19,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
-
+# runtime: 12.790003776550293 seconds
 
 BST = BSTNode(names_1[0])
 for name in names_1:
@@ -27,7 +27,7 @@ for name in names_1:
 for name in names_2:
     if BST.contains(name):
         duplicates.append(name)
-
+# runtime: 0.16200661659240723 seconds
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
