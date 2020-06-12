7b62e119d9101ab7e1f7d599720882b49d10be07
immxalan <aedocoding@gmail.com>
2020-06-12 09:46:36 -0700

    MVP done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,10 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.data = [None for i in range(capacity)]
+        self.capacity = capacity
+        self.old = None
+        self.next_oldest = None
+        self.data = []
 
     def append(self, item):
-        self.data.pop(0)
-        self.data.append(item)
+        if self.old is None:
+            self.old = 0
+            return self.data.append(item)
+    
+        elif len(self.data) + 1 <= self.capacity:
+            self.next_oldest = 1
+            return self.data.append(item)
+
+        else:
+            self.data[self.old] = item
+            self.old += 1 
+        if self.old + 1  > self.capacity:
+            self.old = 0
 
     def get(self):
         return self.data
